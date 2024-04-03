172f9db336a1d5a718e48f017efc32842002ca17
perite <nikita.krementar@gmail.com>
2024-04-03 13:19:40 +0200

    Added redirect from "/", Added redirect to ?week= if week wasnt specified, Added function to find first and last days of given week Design changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import datetime
 
 import sys
@@ -11,14 +11,35 @@ import database
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return redirect("/sessions")
+
+
 @app.route('/sessions')
 def one_week_sessions():
     request_dict = request.args.to_dict()
-    week_number = request_dict.get("week") or datetime.datetime.now().isocalendar()[1]
+    if not (week_number := request_dict.get("week")):
+        week_number = datetime.datetime.now().isocalendar()[1]
+        return redirect(f'/sessions?week={week_number}')
 
+    week_number = int(week_number)
     sessions = database.get_session_by_week(week_number)
+    first_last_dates = get_week_dates(datetime.datetime.now().year, week_number)
 
-    return render_template("one_week_sessions.html", sessions=sessions, status_colors =web_confg.status_colors )
+    return render_template("one_week_sessions.html",
+                           sessions=sessions,
+                           status_colors=web_confg.status_colors,
+                           enumerate=enumerate,
+                           week_number=week_number,
+                           first_last_dates=first_last_dates)
+
+
+def get_week_dates(year, week_number):
+    first_day = datetime.datetime(year, 1, 1)
+    first_day_of_week = first_day + datetime.timedelta(days=(week_number - 1) * 7 - first_day.weekday())
+    last_day_of_week = first_day_of_week + datetime.timedelta(days=6)
+    return (f"{first_day_of_week:%d.%m}", f"{last_day_of_week:%d.%m}")
 
 
 if __name__ == '__main__':
