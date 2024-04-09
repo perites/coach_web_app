eb5ac76a25da7188de3287a52f26f97196572067
perite <nikita.krementar@gmail.com>
2024-04-09 20:51:14 +0200

    added page for groups sessions

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import web_confg
 
 sys.path.append(web_confg.PATH_TO_DB_MODELS)
 import database
+import confg
 
 app = Flask(__name__)
 
@@ -32,7 +33,29 @@ def one_week_sessions():
                            status_colors=web_confg.status_colors,
                            enumerate=enumerate,
                            week_number=week_number,
-                           first_last_dates=first_last_dates)
+                           first_last_dates=first_last_dates,
+                           )
+
+
+@app.route('/groups')
+def one_week_group_sessions():
+    request_dict = request.args.to_dict()
+    if not (week_number := request_dict.get("week")):
+        week_number = datetime.datetime.now().isocalendar()[1]
+        return redirect(f'/groups?week={week_number}')
+
+    week_number = int(week_number)
+    sessions, clients = database.get_session_by_week(week_number, groups=True)
+    first_last_dates = get_week_dates(datetime.datetime.now().year, week_number)
+
+    return render_template("one_week_group_sessions.html",
+                           sessions=sessions,
+                           group_status_colors=web_confg.group_status_colors,
+                           enumerate=enumerate,
+                           week_number=week_number,
+                           first_last_dates=first_last_dates,
+                           clients=clients,
+                           min_participants=confg.MIN_AMOUNT_FOR_GROUP_SESSION)
 
 
 def get_week_dates(year, week_number):
